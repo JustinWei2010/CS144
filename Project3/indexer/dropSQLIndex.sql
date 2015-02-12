dd88c23f6f6c2b5cf78ea518bf04f1950af732c7
Justin Wei <hottfro@gmail.com>
2015-02-11 22:57:37 -0800

    Added in submitted zip files

--- Project3/indexer/dropSQLIndex.sql
+++ Project3/indexer/dropSQLIndex.sql
@@ -1,3 +1,3 @@
-#might have to check if table exists before dropping index
-ALTER TABLE item_location DROP INDEX coord_sp_index;
+#drop table auto drops index, this is to avoid errors when table does not exist
+#ALTER TABLE item_location DROP INDEX coord_sp_index;
 DROP TABLE IF EXISTS item_location;
